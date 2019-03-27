900158c8600bebb522e8c64200023eef85917642
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-03-27 18:23:02 +0700

    clean code

--- src/main/java/com/mrhmt/controllers/AdminCategoryController.java
+++ src/main/java/com/mrhmt/controllers/AdminCategoryController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import sun.misc.Request;
 
 import javax.validation.Valid;
 import java.util.List;
--- src/main/java/com/mrhmt/controllers/AuthenticationController.java
+++ src/main/java/com/mrhmt/controllers/AuthenticationController.java
@@ -6,7 +6,6 @@ import com.mrhmt.repositories.RoleRepository;
 import com.mrhmt.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
